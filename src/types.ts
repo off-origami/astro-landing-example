94f00b6009eea681b49a710277f7ddbb6e559b6f
off-origami <baskakovihor@gmail.com>
2024-11-18 04:26:57 -0800

    changed structure of types in types.ts and added essential info in config.ts

--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,33 @@
 import type socialIcons from "@assets/socialIcons";
 
-export type Page = {
-  TITLE: string;
-  DESCRIPTION: string;
-};
+export type Site = {
+  URL: string; // Canonical URL
+  NAME: string; // Site name
+  TITLE: string; // Default title for meta tags
+  DESCRIPTION: string; // Default description for meta tags
+  IMAGE: string; // Default image for meta tags
+  IMAGE_ALT: string; // Alt text for the image
 
-export interface Site extends Page {
-  WEBSITE: string;
-  AUTHOR: string;
-  OGIMAGE?: string;
-  LIGHTANDDARKMODE: boolean;
-}
+  OG?: {
+    TITLE?: string;
+    DESCRIPTION?: string;
+    IMAGE?: string;
+    IMAGE_ALT?: string;
+  };
+
+  TWITTER?: {
+    CARD?: "summary" | "summary_large_image" | "app" | "player"; // Twitter Card type
+    TITLE?: string;
+    DESCRIPTION?: string;
+    IMAGE?: string;
+    IMAGE_ALT?: string;
+    CREATOR?: string; // Twitter handle of the creator
+  };
+};
 
 export type Socials = {
   NAME: keyof typeof socialIcons;
   HREF: string;
-  ACTIVE: boolean;
   LINKTITLE: string;
+  ACTIVE: boolean;
 }[];
