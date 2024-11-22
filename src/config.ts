dc93b3ca770010a88336ad55a2eb4b98520b0b25
off-origami <baskakovihor@gmail.com>
2024-11-22 07:53:55 -0800

    -fixed names for types and config -.ts, from all CAPS to camel

--- src/config.ts
+++ src/config.ts
@@ -1,44 +1,44 @@
 import type { Site, Socials } from "@types";
 
 export const SITE: Site = {
-  URL: "https://example.com",
-  NAME: "pagegen",
-  TITLE: "Pagegen: Webpage builder for marketers",
-  DESCRIPTION:
+  url: "https://example.com",
+  name: "pagegen",
+  title: "Pagegen: Webpage builder for marketers",
+  description:
     "Pagegen is a platform designed for marketing teams to quickly create, test, and optimize high-converting webpages without the need for coding.",
-  IMAGE: "/default-og-image.jpg",
-  IMAGE_ALT: "A preview of Example Site",
+  image: "/default-og-image.jpg",
+  imageAlt: "A preview of Example Site",
 
-  TWITTER: {
-    CARD: "summary_large_image",
-    CREATOR: "@odelyonweb",
-    IMAGE: "/custom-twitter-image.jpg",
+  twitter: {
+    card: "summary_large_image",
+    creator: "@odelyonweb",
+    image: "/custom-twitter-image.jpg",
   },
 };
 
 export const SOCIALS: Socials = [
   {
-    NAME: "Github",
-    HREF: "https://github.com/satnaing/astro-paper",
-    LINKTITLE: ` ${SITE.title} on Github`,
-    ACTIVE: true,
+    name: "Github",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Github`,
+    active: true,
   },
   {
-    NAME: "Facebook",
-    HREF: "https://github.com/satnaing/astro-paper",
-    LINKTITLE: `${SITE.title} on Facebook`,
-    ACTIVE: true,
+    name: "Facebook",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on Facebook`,
+    active: true,
   },
   {
-    NAME: "LinkedIn",
-    HREF: "https://github.com/satnaing/astro-paper",
-    LINKTITLE: `${SITE.title} on LinkedIn`,
-    ACTIVE: true,
+    name: "LinkedIn",
+    href: "https://github.com/satnaing/astro-paper",
+    linkTitle: `${SITE.title} on LinkedIn`,
+    active: true,
   },
   {
-    NAME: "Mail",
-    HREF: "mailto:yourmail@gmail.com",
-    LINKTITLE: `Send an email to ${SITE.title}`,
-    ACTIVE: false,
+    name: "Mail",
+    href: "mailto:yourmail@gmail.com",
+    linkTitle: `Send an email to ${SITE.title}`,
+    active: false,
   },
 ];
--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,33 @@
 import type socialIcons from "@assets/socialIcons";
 
 export type Site = {
-  URL: string; // Canonical URL
-  NAME: string; // Site name
-  TITLE: string; // Default title for meta tags
-  DESCRIPTION: string; // Default description for meta tags
-  IMAGE: string; // Default image for meta tags
-  IMAGE_ALT: string; // Alt text for the image
+  url: string; // Canonical URL
+  name: string; // Site name
+  title: string; // Default title for meta tags
+  description: string; // Default description for meta tags
+  image: string; // Default image for meta tags
+  imageAlt: string; // Alt text for the image
 
-  OG?: {
-    TITLE?: string;
-    DESCRIPTION?: string;
-    IMAGE?: string;
-    IMAGE_ALT?: string;
+  og?: {
+    title?: string;
+    description?: string;
+    image?: string;
+    imageAlt?: string;
   };
 
-  TWITTER?: {
-    CARD?: "summary" | "summary_large_image" | "app" | "player"; // Twitter Card type
-    TITLE?: string;
-    DESCRIPTION?: string;
-    IMAGE?: string;
-    IMAGE_ALT?: string;
-    CREATOR?: string; // Twitter handle of the creator
+  twitter?: {
+    card?: "summary" | "summary_large_image" | "app" | "player"; // Twitter Card type
+    title?: string;
+    description?: string;
+    image?: string;
+    imageAlt?: string;
+    creator?: string; // Twitter handle of the creator
   };
 };
 
 export type Socials = {
-  NAME: keyof typeof socialIcons;
-  HREF: string;
-  LINKTITLE: string;
-  ACTIVE: boolean;
+  name: keyof typeof socialIcons;
+  href: string;
+  linkTitle: string;
+  active: boolean;
 }[];
